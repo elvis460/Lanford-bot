a228c0b0689dd4bbee90676a9f6e8733f96cbbb6
Villager <elvis4600@gmail.com>
2016-07-15 15:41:50 +0800

    bot ver2

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -17,111 +17,59 @@ class BotsController < ApplicationController
         if (event[:message] && text = event[:message][:text])
           if (event[:message][:quick_reply] && payload = event[:message][:quick_reply][:payload])
             case payload
+              when 'Greet'
+                FacebookBot.new.default_message(sender)
+              when 'Skills'
+                FacebookBot.new.send_text_message(sender, '等等呢，我還在學習...')  
+              when 'Shoes'
+                FacebookBot.new.sale_shoes(sender)
               when '早安'
-                image = {
-                  "attachment":{
-                    "type":"image",
-                    "payload":{
-                      "url":"http://front-pic.style.fashionguide.com.tw/uploads/share/picture/222352/share_picture_1359999787.jpg"
-                    }
-                  }
-                }
-                FacebookBot.new.send_generic_message(sender, image)
+                FacebookBot.new.good_morning(sender)
               when '午安'
-                option = {
-                  attachment: {
-                    type: "template",
-                    payload: {
-                      template_type: "generic",
-                        elements: [{
-                        title: "藍佛是我",
-                        subtitle: "我是藍佛",
-                        item_url: "https://www.facebook.com/yiwen.lan.10",               
-                        image_url: "https://www.villager.website/assets/lanford_2-f2dec8ef23df59c06ce0f4a02c6f8741cdf94f8aebe2ede16f7772866b3d5118.jpg",
-                        buttons: [{
-                          type: "web_url",
-                          url: "https://www.facebook.com/yiwen.lan.10",
-                          title: "誠徵男友"
-                        }, {
-                          type: "postback",
-                          title: "跟我聊",
-                          payload: "TalkToMe"
-                        }],
-                      }, {
-                        title: "Lanford is me",
-                        subtitle: "I'm Lanford",
-                        item_url: "https://www.facebook.com/yiwen.lan.10",               
-                        image_url: "https://www.villager.website/assets/lanford_1-bb6b5025f112643b61739e620b7ab07063ea19b4d2e07cf76a5d13e5db77ff4e.jpg",
-                        buttons: [{
-                          type: "web_url",
-                          url: "https://www.facebook.com/yiwen.lan.10",
-                          title: "See Me !!!"
-                        }, {
-                          type: "postback",
-                          title: "跟我聊",
-                          payload: "TalkToMe"
-                        }]
-                      }]
-                    }
-                  }
-                }
-                FacebookBot.new.send_generic_message(sender, option)  
+                FacebookBot.new.good_afternoon(sender)  
               when '晚安'
-                button = {
-                  "attachment":{
-                    "type": "template",
-                    "payload":{
-                      "template_type": "button",
-                      "text": "我是藍佛，藍佛是我",
-                      "buttons":[
-                        {
-                          "type": "web_url",
-                          "url": "https://www.facebook.com/yiwen.lan.10",
-                          "title": "看我看我"
-                        },
-                        {
-                          "type": "postback",
-                          "title": "跟我聊",
-                          "payload": "TalkToMe"
-                        }
-                      ]
-                    }
-                  }
-                }  
-                FacebookBot.new.send_generic_message(sender, button) 
+                FacebookBot.new.good_night(sender) 
             end
           else
             case text
               when 'hello'
-                FacebookBot.new.send_text_message(sender, "I'm Lanford.")               
+                FacebookBot.new.send_text_message(sender, "I'm Lanford.") 
+              when '鞋子'
+                FacebookBot.new.sale_shoes(sender)     
               else
-                mes = {
-                  "text": "嗨～我是藍佛:",
-                  "quick_replies":[
-                    {
-                      "content_type": "text",
-                      "title": "早安",
-                      "payload": "早安"
-                    },
-                    {
-                      "content_type": "text",
-                      "title": "午安",
-                      "payload": "午安"
-                    },
-                    {
-                      "content_type": "text",
-                      "title": "晚安",
-                      "payload": "晚安"
-                    }
-                  ]
-                }
-                FacebookBot.new.send_generic_message(sender, mes)
+                FacebookBot.new.default_message(sender) 
             end
           end
         elsif(event[:postback] && postback = event[:postback][:payload])
           case postback
-            when 'TalkToMe'
+            when 'Greet'
               FacebookBot.new.default_message(sender)
+            when 'Skills'
+              FacebookBot.new.send_text_message(sender, '等等呢，我還在學習...')  
+            when 'Start'
+              start = {
+                "text": "我是藍佛，有何貴幹？",
+                "quick_replies":[
+                  {
+                    "content_type": "text",
+                    "title": "打個招呼",
+                    "payload": "Greet"
+                  },
+                  {
+                    "content_type": "text",
+                    "title": "買個鞋吧",
+                    "payload": "Shoes"
+                  },
+                  {
+                    "content_type": "text",
+                    "title": "我想看妳會什麼",
+                    "payload": "Skills"
+                  },
+                ]
+              }
+              FacebookBot.new.send_generic_message(sender, start)
+            when 'Shoes'
+              FacebookBot.new.sale_shoes(sender)
           end 
         end        
       end
--- app/models/facebook_bot.rb
+++ app/models/facebook_bot.rb
@@ -57,4 +57,122 @@ class FacebookBot
     send_generic_message(sender, mes)
   end
 
+  def sale_shoes(sender)
+    shoes = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+            elements: [{
+            title: "彩色勾勾",
+            subtitle: "彩色Nike",
+            item_url: "https://www.google.com.tw/search?q=%E9%9E%8B%E5%AD%90&espv=2&biw=1280&bih=729&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjey_bA8fTNAhUJJpQKHbkBCBQQ_AUIBigB#imgrc=QNFl4XNYQEEXKM%3A",               
+            image_url: "http://down1.sucaitianxia.com/ai/20/ai4095.jpg",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.google.com.tw/search?q=%E9%9E%8B%E5%AD%90&espv=2&biw=1280&bih=729&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjey_bA8fTNAhUJJpQKHbkBCBQQ_AUIBigB#imgrc=QNFl4XNYQEEXKM%3A",
+              title: "前往選購"
+            }, {
+              type: "postback",
+              title: "讓我看妳還會什麼",
+              payload: "Skills"
+            }],
+          }, {
+            title: "踢不爛",
+            subtitle: "Timberland",
+            item_url: "https://www.google.com.tw/search?espv=2&biw=1280&bih=685&tbm=isch&sa=1&q=timberland&oq=Timber&gs_l=img.3.0.0l10.139579.141876.0.142513.13.11.1.1.1.0.57.473.11.11.0....0...1c.1j4.64.img..0.8.276.aiodn-bjRbc#imgrc=aNE7b6kAk11o4M%3A",               
+            image_url: "https://images.timberland.com/is/image/timberland/10061024-HERO?$PLP-IMAGE$",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.google.com.tw/search?espv=2&biw=1280&bih=685&tbm=isch&sa=1&q=timberland&oq=Timber&gs_l=img.3.0.0l10.139579.141876.0.142513.13.11.1.1.1.0.57.473.11.11.0....0...1c.1j4.64.img..0.8.276.aiodn-bjRbc#imgrc=aNE7b6kAk11o4M%3A",
+              title: "前往選購"
+            }, {
+              type: "postback",
+              title: "還會別的？",
+              payload: "Skills"
+            }]
+          }]
+        }
+      }
+    }
+    send_generic_message(sender, shoes)    
+  end
+
+  def good_morning(sender)
+    image = {
+      "attachment":{
+        "type":"image",
+        "payload":{
+          "url":"http://front-pic.style.fashionguide.com.tw/uploads/share/picture/222352/share_picture_1359999787.jpg"
+        }
+      }
+    }
+    send_generic_message(sender, image)
+  end
+
+  def good_afternoon(sender)
+    option = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+            elements: [{
+            title: "藍佛是我",
+            subtitle: "我是藍佛",
+            item_url: "https://www.facebook.com/yiwen.lan.10",               
+            image_url: "https://www.villager.website/assets/lanford_2-f2dec8ef23df59c06ce0f4a02c6f8741cdf94f8aebe2ede16f7772866b3d5118.jpg",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.facebook.com/yiwen.lan.10",
+              title: "誠徵男友"
+            }, {
+              type: "postback",
+              title: "跟我聊",
+              payload: "Greet"
+            }],
+          }, {
+            title: "Lanford is me",
+            subtitle: "I'm Lanford",
+            item_url: "https://www.facebook.com/yiwen.lan.10",               
+            image_url: "https://www.villager.website/assets/lanford_1-bb6b5025f112643b61739e620b7ab07063ea19b4d2e07cf76a5d13e5db77ff4e.jpg",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.facebook.com/yiwen.lan.10",
+              title: "See Me !!!"
+            }, {
+              type: "postback",
+              title: "跟我聊",
+              payload: "Greet"
+            }]
+          }]
+        }
+      }
+    }
+    send_generic_message(sender, option)
+  end
+
+  def good_night(sender)
+    button = {
+      "attachment":{
+        "type": "template",
+        "payload":{
+          "template_type": "button",
+          "text": "我是藍佛，藍佛是我",
+          "buttons":[
+            {
+              "type": "web_url",
+              "url": "https://www.facebook.com/yiwen.lan.10",
+              "title": "看我看我"
+            },
+            {
+              "type": "postback",
+              "title": "跟我聊",
+              "payload": "Greet"
+            }
+          ]
+        }
+      }
+    }  
+    send_generic_message(sender, button) 
+  end
 end
